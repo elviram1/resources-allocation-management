0f570e32442e94b910b00f915208d29c1771fbad
Elvira <elvimaz@gmail.com>
2022-02-24 12:14:43 +0100

    rework getAllProject and ProjectView.html

--- src/main/java/com/example/ram/api/ProjectController.java
+++ src/main/java/com/example/ram/api/ProjectController.java
@@ -2,14 +2,19 @@ package com.example.ram.api;
 
 import com.example.ram.model.Project;
 import com.example.ram.service.ProjectService;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
-@RequestMapping("api/v1/project")
+
+@RequestMapping("/projects")
 @RestController
 public class ProjectController {
 
@@ -21,12 +26,11 @@ public class ProjectController {
     }
 
     @GetMapping
-    public List<Project> getAllProject(){
-        return projectService.getAllProject();
+    public ModelAndView getAllProject(){
+        ModelAndView mav = new ModelAndView("ProjectView");
+        List<Project> projectList = projectService.getAllProject();
+        mav.addObject("projects", projectList);
+        return mav;
     }
-
-
-
-
 
 }
